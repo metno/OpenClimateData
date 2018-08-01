ca37efc42f0bc58f2b2ddeeab15cf999323c414d
Rasmus Benestad <rasmusb@pc4321.pc.met.no>
2018-08-01 15:09:02 +0200

    further development

--- set-up.sh
+++ set-up.sh
@@ -4,5 +4,8 @@ cat > set-up.R << EOF
   install_github('metno/esd')
 EOF
 
+#sudo apt-get install -f
+#sudo apt-get install libnetcdf-dev
+#sudo apt-get install liblapack-dev
 R --slave --no-restore -e 'source("set-up.R")'
 ln -s OpenClimateData/launch.sh 
