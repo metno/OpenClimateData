cffe4e606bcbfc7a7773211dd92b95c2d1006b84
Rasmus Benestad <rasmusb@pc5055.pc.met.no>
2018-12-10 13:55:06 +0100

    further development

--- set-up.sh
+++ set-up.sh
@@ -1,11 +1,16 @@
+#/bin/bash
+sudo apt-get install -f
+sudo apt-get install libnetcdf-dev
+sudo apt-get install liblapack-dev
+sudo apt-get install libssl-dev
+sudo apt-get install libgit2-dev
+
+
 cat > set-up.R << EOF
   install.packages(c('devtools','shiny','shinydashboard','leaflet','plotly'),repos='http://cran.uib.no')
   library(devtools)
   install_github('metno/esd')
 EOF
 
-#sudo apt-get install -f
-#sudo apt-get install libnetcdf-dev
-#sudo apt-get install liblapack-dev
 R --slave --no-restore -e 'source("set-up.R")'
 ln -s OpenClimateData/launch.sh 
