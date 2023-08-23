57195aa6c6821e7978a6966edb06621062421297
Christian Skarby <christian.skarby@met.no>
2023-08-23 12:29:43 +0200

    Make data directory available under app (for compliance with local development / RStudio)

--- pullfiles.sh
+++ pullfiles.sh
@@ -1,2 +1,3 @@
-wget -N -e robots=off -nH --cut-dirs 4 -r -l5 -A '*.metnod.nc' -R 'catalog*' -I /thredds/fileServer/,/thredds/catalog/ 'https://thredds.met.no/thredds/catalog/metusers/rasmusb/catalog.html' -P ~/OpenClimateData/data
-
+#!/bin/sh
+cd $(dirname $0) && \
+wget -N -e robots=off -nH --cut-dirs 4 -r -l5 -A '*.metnod.nc' -R 'catalog*' -I /thredds/fileServer/,/thredds/catalog/ 'https://thredds.met.no/thredds/catalog/metusers/rasmusb/catalog.html' -P ./data
