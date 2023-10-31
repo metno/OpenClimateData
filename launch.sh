b8f947d9058e969e0c18c86f4151d8f77b33a82e
Christian Skarby <christian.skarby@met.no>
2023-10-31 09:52:38 +0100

    Fix spelling error

--- launch.sh
+++ launch.sh
@@ -9,7 +9,7 @@ set -e
 cd $(dirname $0) >/dev/null 2>&1 || exit $?
 podman play kube --replace podman.yaml
 echo -n "shiny-server is starting up "
-STATUS="WATING"; I=0; while [[ "$STATUS" != "200" ]]; do
+STATUS="WAITING"; I=0; while [[ "$STATUS" != "200" ]]; do
   sleep 1;
   STATUS=$(curl -qsI "$URL" | head -n1 | awk '{print $2}')
   if [ $(($I % 10)) -eq 0 ]; then
